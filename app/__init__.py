b8896dbcaf642a32945c1a9145ddbe80e8c7bbc3
gizmo <gizmo.engineer8878@gmail.com>
2026-03-09 04:13:25 +0900

    fix flask template path

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask
-from app.config import Config
-from app.extensions import engine, Base
-
+import os
 
 def create_app():
 
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    template_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../templates")
+    )
 
-    from app.routes.tracks import tracks_bp
-    from app.routes.auth import auth_bp
-
-    app.register_blueprint(tracks_bp)
-    app.register_blueprint(auth_bp)
-
-    Base.metadata.create_all(bind=engine)
+    app = Flask(
+        __name__,
+        template_folder=template_dir
+    )
 
     return app
